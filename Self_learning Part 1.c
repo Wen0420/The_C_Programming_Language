92618c972db2c326525586af9e8c6aa3a4583176
K.T <55773448+Wen0420@users.noreply.github.com>
2021-06-11 13:05:07 +1000

    Add files via upload

--- Self_learning Part 1.c
+++ Self_learning Part 1.c
@@ -183,17 +183,27 @@
 //1  64
 //1  81
 
+//int main()
+//{
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//	int i = 0;
+//	while (i < 10)
+//	{printf("%d ", arr[i]);
+//	i++;
+//}
+//
+//	return 0;
+//}
+
 int main()
 {
-	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-	int i = 0;
-	while (i < 10)
-	{printf("%d ", arr[i]);
-	i++;
-}
-
+	int a = 1;
+	int b = a << 2;
+	printf("%d\n", b);
 	return 0;
 }
+
+
 
 
 
