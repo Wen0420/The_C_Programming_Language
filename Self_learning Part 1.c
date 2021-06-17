4422d1ca6b6356bfcc85ff8746545f0b8bd8b71f
K.T <55773448+Wen0420@users.noreply.github.com>
2021-06-17 15:54:17 +1000

    Add files via upload

--- Self_learning Part 1.c
+++ Self_learning Part 1.c
@@ -323,17 +323,33 @@
 //	return 0;
 //}//The single number is :  5
 
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,1,2,3,4 };
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(arr[0]));
+//	printf("%d\n", sz);
+//
+//	return 0;
+//}// 36 4 9
+
 int main()
 {
 	int arr[] = { 1,2,3,4,5,1,2,3,4 };
 	int i = 0;
+	int ret = 0;
 	int sz = sizeof(arr) / sizeof(arr[0]);
-	printf("%d\n", sizeof(arr));
-	printf("%d\n", sizeof(arr[0]));
-	printf("%d\n", sz);
+	for (i = 0; i < sz; i++)
+	{
+		ret = ret ^ arr[i];
+	}
+	printf("The single number is : % d\n", ret);
 
 	return 0;
-}//The single number is :  5
+}// The single number is :  5
+
 
 
 
