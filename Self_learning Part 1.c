8f545c29d85633db2b54130adb6ccd389d560039
K.T <55773448+Wen0420@users.noreply.github.com>
2021-06-17 15:11:32 +1000

    Add files via upload

--- Self_learning Part 1.c
+++ Self_learning Part 1.c
@@ -297,7 +297,31 @@
 //}//before change: a = 3 b = 5
 ////after change : a = 5 b = 3
 
+//Leetcode 136
 
+int main()
+{
+	int arr[] = { 1,2,3,4,5,1,2,3,4 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++)
+	{
+		int count = 0;
+			int j = 0;
+			for (j = 0; j < sz; j++)
+			{
+				if (arr[i] == arr[j])
+					count++;
+			}
+			if (count == 1)
+			{
+				printf("The single number is : % d\n", arr[i]);
+				break;
+			}
+	}
+
+	return 0;
+}//The single number is :  5
 
 
 
