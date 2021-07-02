5c90fff76abd5149d8bd05b0bef94986ecd72a7a
K.T <55773448+Wen0420@users.noreply.github.com>
2021-07-02 18:37:22 +1000

    Add files via upload

--- Self_learning Part 2.c
+++ Self_learning Part 2.c
@@ -1,8 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-int main()
-{
-	int num = 8;
-	printf("%d\n", num);
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int num = 8;
+//	printf("%d\n", num);
+//	return 0;
+//}
